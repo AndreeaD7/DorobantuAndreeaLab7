083fee970c0cb96744c8269576c5d0458026c2a7
AndreeaD7 <andreea.dorobantu98@gmail.com>
2024-12-12 21:11:04 +0200

    Lab8

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,27 @@
-﻿namespace DorobantuAndreeaLab7
+﻿using DorobantuAndreeaLab7.Data;
+
+
+namespace DorobantuAndreeaLab7
 {
+
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+    ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+    LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
+
         public App()
         {
             InitializeComponent();
